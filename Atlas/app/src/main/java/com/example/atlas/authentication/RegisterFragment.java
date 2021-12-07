f4202106e45ab9bcb24089064b8dcc8dda5c46b6
shradhakedia <shradhakedia1998@gmail.com>
2021-12-07 21:27:54 +0530

    connect fragment to each other

--- Atlas/app/src/main/java/com/example/atlas/authentication/RegisterFragment.java
+++ Atlas/app/src/main/java/com/example/atlas/authentication/RegisterFragment.java
@@ -2,11 +2,14 @@ package com.example.atlas.authentication;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.atlas.R;
 
@@ -16,11 +19,26 @@ import com.example.atlas.R;
  */
 public class RegisterFragment extends Fragment {
 
+    TextView goToLogin;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        goToLogin = (TextView) view.findViewById(R.id.tv_login);
+
+        goToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.fr)
+            }
+        });
     }
 }
