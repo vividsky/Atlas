74d35a56f87c07f3b126580631f2590f34f1aed9
shradhakedia <shradhakedia1998@gmail.com>
2021-12-10 14:49:43 +0530

    retain the UserDetails while coming back from profile page.

--- Atlas/app/src/main/java/com/example/atlas/MainActivity.java
+++ Atlas/app/src/main/java/com/example/atlas/MainActivity.java
@@ -1,20 +1,8 @@
 package com.example.atlas;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
-import android.widget.Toast;
 
-import com.example.atlas.Models.User;
-import com.example.atlas.authentication.AuthenticationActivity;
-import com.example.atlas.authentication.UserDetailsFragment;
-import com.example.atlas.authentication.UserProfileFragment;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,11 +11,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        // TODO user should not go back to user profile fragment and exits the apps
     }
 }
--- Atlas/app/src/main/java/com/example/atlas/authentication/UserDetailsFragment.java
+++ Atlas/app/src/main/java/com/example/atlas/authentication/UserDetailsFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
@@ -31,6 +32,11 @@ public class UserDetailsFragment extends Fragment {
 
     private static final String TAG = UserDetailsFragment.class.getSimpleName();
 
+    private static final String NAME = "Name";
+    private static final String GENDER = "Gender";
+    private static final String ADDRESS = "Address";
+    private static final String ALTERNATE_CONTACT = "alternate contact";
+
     private static final String GENDER_MALE = "Male";
     private static final String GENDER_FEMALE = "Female";
 
@@ -40,6 +46,11 @@ public class UserDetailsFragment extends Fragment {
     private RadioGroup mGender;
     private Button mSave;
 
+    String name;
+    String address;
+    String alternateContact;
+    String[] gender = new String[]{GENDER_MALE};
+
     private FirebaseAuth firebaseAuth;
     private FirebaseFirestore firebaseFirestore;
 
@@ -48,6 +59,7 @@ public class UserDetailsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_user_details, container, false);
+
     }
 
     @Override
@@ -58,10 +70,18 @@ public class UserDetailsFragment extends Fragment {
         mName = (EditText) view.findViewById(R.id.et_name);
         mAddress = (EditText) view.findViewById(R.id.et_address);
         mAlternateContact = (EditText) view.findViewById(R.id.et_contact_alternate);
-
         mGender = (RadioGroup) view.findViewById(R.id.rg_gender);
+        mSave = (Button) view.findViewById(R.id.bv_save);
 
-        String[] gender = new String[1];
+
+        if (savedInstanceState != null) {
+            mName.setText(name);
+            mAddress.setText(address);
+            mAlternateContact.setText(alternateContact);
+            if(gender[0].equals(GENDER_FEMALE)) {
+                ((RadioButton) mGender.getChildAt(0)).setChecked(true);
+            }
+        }
 
         // updating gender on radio button click
         mGender.setOnCheckedChangeListener((group, id) -> {
@@ -73,100 +93,106 @@ public class UserDetailsFragment extends Fragment {
             }
         });
 
-        mSave = (Button) view.findViewById(R.id.bv_save);
-
         // validation check will be performed when Sign up button is clicked
-        mSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        mSave.setOnClickListener(v -> {
 
-                String name = mName.getText().toString();
-                String address = mAddress.getText().toString();
-                String alternateContact = mAlternateContact.getText().toString();
+            // getting the texts from Views
+            name = mName.getText().toString();
+            address = mAddress.getText().toString();
+            alternateContact = mAlternateContact.getText().toString();
 
-                // setting it to null so that after filling any data previous error is gone
-                mName.setError(null);
-                mAddress.setError(null);
-                mAlternateContact.setError(null);
+            // setting it to null so that after filling any data previous error is gone
+            mName.setError(null);
+            mAddress.setError(null);
+            mAlternateContact.setError(null);
 
-                /* validations check
-                 * 1. name
-                 * 2. gender
-                 * 3. address
-                 * 4. alternate contact
-                 * 5. services
-                */
-                if (TextUtils.isEmpty(name)) {
-                    mName.setError("Name is required.");
-                    return;
-                }
-                if (!Pattern.compile("^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$").matcher(name).matches()) {
-                    mName.setError("Please enter a valid name.");
-                    return;
-                }
-
-                if (mGender.getCheckedRadioButtonId() == -1) {
-                    Toast.makeText(getContext(), "Gender is required.", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-
-                if (TextUtils.isEmpty(address)) {
-                    mAddress.setError("Address is required.");
-                    return;
-                }
-
-                if (!TextUtils.isEmpty(alternateContact) && !Pattern.compile("^(\\+\\d{1,3}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$")
-                        .matcher(alternateContact).matches()) {
-                    mAlternateContact.setError("Please enter a valid Contact detail.");
-                    return;
-                }
-
-                firebaseAuth = FirebaseAuth.getInstance();
-                firebaseFirestore = FirebaseFirestore.getInstance();
-
-                // getting the document users by its id because its unique always
-                DocumentReference user = firebaseFirestore.collection("Users").document(firebaseAuth.getCurrentUser().getUid());
-
-                /* Updates
-                 * 1. name
-                 * 2. gender
-                 * 3. address
-                 * 4. alternate contact
-                 */
-                user.update("name", name).addOnSuccessListener(success -> {
-                    Log.d(TAG, "name successfully updated");
-                }).addOnFailureListener(failure -> {
-                    Log.d(TAG, "name updating failed");
-                });
-
-                user.update("gender", gender[0]).addOnSuccessListener(success -> {
-                    Log.d(TAG, "gender successfully updated");
-                }).addOnFailureListener(failure -> {
-                    Log.d(TAG, "gender updating failed");
-                });
-
-                user.update("address", address).addOnSuccessListener(success -> {
-                    Log.d(TAG, "address successfully updated");
-                }).addOnFailureListener(failure -> {
-                    Log.d(TAG, "address updating failed");
-                });
-
-                user.update("alternateContact", alternateContact).addOnSuccessListener(success -> {
-                    Log.d(TAG, "alternate Contact successfully updated");
-                }).addOnFailureListener(failure -> {
-                    Log.d(TAG, "alternate Contact updating failed");
-                });
-
-                // to indicate user save was successful
-                Toast.makeText(getContext(), "Successfully Saved", Toast.LENGTH_SHORT).show();
-                getParentFragmentManager().beginTransaction()
-                        .replace(R.id.auth_fragment_container, new UserProfileFragment())
-                        .commit();
-
+            /* validations check
+             * 1. name
+             * 2. gender
+             * 3. address
+             * 4. alternate contact
+             * 5. services
+            */
+            if (TextUtils.isEmpty(name)) {
+                mName.setError("Name is required.");
+                return;
             }
+            if (!Pattern.compile("^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$").matcher(name).matches()) {
+                mName.setError("Please enter a valid name.");
+                return;
+            }
+
+            if (mGender.getCheckedRadioButtonId() == -1) {
+                Toast.makeText(getContext(), "Gender is required.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+
+            if (TextUtils.isEmpty(address)) {
+                mAddress.setError("Address is required.");
+                return;
+            }
+
+            if (!TextUtils.isEmpty(alternateContact) && !Pattern.compile("^(\\+\\d{1,3}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$")
+                    .matcher(alternateContact).matches()) {
+                mAlternateContact.setError("Please enter a valid Contact detail.");
+                return;
+            }
+
+            firebaseAuth = FirebaseAuth.getInstance();
+            firebaseFirestore = FirebaseFirestore.getInstance();
+
+            // getting the document users by its id because its unique always
+            DocumentReference user = firebaseFirestore.collection("Users").document(firebaseAuth.getCurrentUser().getUid());
+
+            /* Updates
+             * 1. name
+             * 2. gender
+             * 3. address
+             * 4. alternate contact
+             */
+            user.update("name", name).addOnSuccessListener(success -> {
+                Log.d(TAG, "name successfully updated");
+            }).addOnFailureListener(failure -> {
+                Log.d(TAG, "name updating failed");
+            });
+
+            user.update("gender", gender[0]).addOnSuccessListener(success -> {
+                Log.d(TAG, "gender successfully updated");
+            }).addOnFailureListener(failure -> {
+                Log.d(TAG, "gender updating failed");
+            });
+
+            user.update("address", address).addOnSuccessListener(success -> {
+                Log.d(TAG, "address successfully updated");
+            }).addOnFailureListener(failure -> {
+                Log.d(TAG, "address updating failed");
+            });
+
+            user.update("alternateContact", alternateContact).addOnSuccessListener(success -> {
+                Log.d(TAG, "alternate Contact successfully updated");
+            }).addOnFailureListener(failure -> {
+                Log.d(TAG, "alternate Contact updating failed");
+            });
+
+            // to indicate user save was successful
+            Toast.makeText(getContext(), "Successfully Saved", Toast.LENGTH_SHORT).show();
+            getParentFragmentManager().beginTransaction()
+                    .replace(R.id.auth_fragment_container, new UserProfileFragment())
+                    .addToBackStack(null)
+                    .commit();
+
         });
 
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(NAME, name);
+        outState.putString(GENDER, gender[0]);
+        outState.putString(ADDRESS, address);
+        outState.putString(ALTERNATE_CONTACT, alternateContact);
     }
 
 }
