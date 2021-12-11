c4b1fa9897a7add64321e7e17382e1880af10ba5
shradhakedia <shradhakedia1998@gmail.com>
2021-12-11 21:50:10 +0530

    ask user to confirm log out.

--- Atlas/app/src/main/java/com/example/atlas/MainActivity.java
+++ Atlas/app/src/main/java/com/example/atlas/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.atlas;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +11,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
@@ -111,10 +113,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 break;
 
             case R.id.nav_logout:
-                firebaseAuth.signOut();
-                Toast.makeText(this, "Sign out successfully", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(MainActivity.this, AuthenticationActivity.class));
-                finish();
+                new AlertDialog.Builder(this)
+                        .setTitle("Log out")
+                        .setMessage("Are you sure you want to Log out?")
+
+                        // Specifying a listener allows you to take an action before dismissing the dialog.
+                        // The dialog is automatically dismissed when a dialog button is clicked.
+                        .setPositiveButton(android.R.string.yes, (dialog, which) -> {
+                            // Continue with delete operation
+                            firebaseAuth.signOut();
+                            Toast.makeText(this, "Sign out successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(MainActivity.this, AuthenticationActivity.class));
+                            finish();
+                        })
+
+                        // A null listener allows the button to dismiss the dialog and take no further action.
+                        .setNegativeButton(android.R.string.no, null)
+                        .setIcon(R.drawable.ic_baseline_logout_24)
+                        .show();
                 break;
 
             case R.id.nav_delete_account:
