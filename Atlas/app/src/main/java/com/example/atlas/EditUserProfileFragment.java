d9fc660e06ac407f8f0e3d1d53259a9971b0d977
shradhakedia <shradhakedia1998@gmail.com>
2021-12-11 21:26:48 +0530

    modify edit user details fragment

--- Atlas/app/src/main/java/com/example/atlas/EditUserProfileFragment.java
+++ Atlas/app/src/main/java/com/example/atlas/EditUserProfileFragment.java
@@ -9,11 +9,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class EditUserProfileFragment extends Fragment {
+
+    Button mChangePassword;
+    Button mConfirmPassword;
+    EditText mOldPassword;
+    EditText mNewPassword;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,5 +32,26 @@ public class EditUserProfileFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        mChangePassword = view.findViewById(R.id.bv_change_password);
+        mOldPassword = view.findViewById(R.id.et_edit_old_password);
+        mNewPassword = view.findViewById(R.id.et_edit_new_password);
+        mConfirmPassword = view.findViewById(R.id.bv_confirm_password);
+
+        mChangePassword.setOnClickListener(v -> {
+            mChangePassword.setVisibility(View.GONE);
+            mOldPassword.setVisibility(View.VISIBLE);
+            mNewPassword.setVisibility(View.VISIBLE);
+            mConfirmPassword.setVisibility(View.VISIBLE);
+        });
+
+        mConfirmPassword.setOnClickListener(v -> {
+            // TODO: if successfully changed
+
+            mChangePassword.setVisibility(View.VISIBLE);
+            mOldPassword.setVisibility(View.GONE);
+            mNewPassword.setVisibility(View.GONE);
+            mConfirmPassword.setVisibility(View.GONE);
+        });
     }
 }
