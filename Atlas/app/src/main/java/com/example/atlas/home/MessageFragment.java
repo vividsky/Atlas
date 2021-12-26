214c05c133f4d7ef34b606467305a544ffe4b326
shradhakedia <shradhakedia1998@gmail.com>
2021-12-26 17:18:23 +0530

    change extends fragment type

--- Atlas/app/src/main/java/com/example/atlas/home/MessageFragment.java
+++ Atlas/app/src/main/java/com/example/atlas/home/MessageFragment.java
@@ -1,8 +1,7 @@
 package com.example.atlas.home;
 
+import android.app.Fragment;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
