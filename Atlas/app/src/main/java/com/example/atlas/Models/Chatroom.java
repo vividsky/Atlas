734a448b07e997bb97727c4757bfc0d80a2a19fb
shradhakedia <shradhakedia1998@gmail.com>
2021-12-26 17:17:09 +0530

    add chatroom fragment's recycler view and its adapter

--- Atlas/app/src/main/java/com/example/atlas/Models/Chatroom.java
+++ Atlas/app/src/main/java/com/example/atlas/Models/Chatroom.java
@@ -1,4 +1,30 @@
 package com.example.atlas.Models;
 
 public class Chatroom {
+
+    private String name;
+    private String chatId;
+
+    @Override
+    public String toString() {
+        return "Chatroom{" +
+                "name='" + name + '\'' +
+                ", chatId='" + chatId + '\'' +
+                '}';
+    }
+
+    public Chatroom(String name, String chatId) {
+        this.name = name;
+        this.chatId = chatId;
+
+    }
+    public Chatroom() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public String getChatId() {
+        return chatId;
+    }
 }
--- Atlas/app/src/main/java/com/example/atlas/home/ChatRoomFragment.java
+++ Atlas/app/src/main/java/com/example/atlas/home/ChatRoomFragment.java
@@ -2,23 +2,45 @@ package com.example.atlas.home;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.atlas.Models.Chatroom;
 import com.example.atlas.R;
+import com.example.atlas.adapters.ChatroomAdapter;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChatRoomFragment extends Fragment {
+    public static final String TAG = ChatRoomFragment.class.getSimpleName();
+
     Toolbar toolbar;
     DrawerLayout mDrawerLayout;
+    RecyclerView recyclerView;
+    ChatroomAdapter chatroomAdapter;
+    List<Chatroom> chatRoomsArrayList;
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -51,10 +73,47 @@ public class ChatRoomFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_chatroom, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        recyclerView = view.findViewById(R.id.chatroom_recycler_view);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        chatRoomsArrayList = new ArrayList<>();
+        setUpRecyclerView();
+
+    }
+
+    private void setUpRecyclerView() {
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseFirestore.collection(getString(R.string.user))
+                .document(firebaseAuth.getCurrentUser().getUid()).collection(getString(R.string.chatrooms))
+                .get().addOnCompleteListener(task -> {
+                    if(task.isSuccessful()) {
+                        Chatroom chatroom;
+                        for(QueryDocumentSnapshot chatroomQuery: task.getResult()) {
+                            chatroom = chatroomQuery.toObject(Chatroom.class);
+                            chatRoomsArrayList.add(chatroom);
+                        }
+                        chatroomAdapter = new ChatroomAdapter(chatRoomsArrayList, getContext());
+                        recyclerView.setHasFixedSize(true);
+                        recyclerView.setAdapter(chatroomAdapter);
+                    } else {
+                        Log.i(TAG, task.getException().getMessage());
+                    }
+        });
+
+    }
+
     // hide the menu item refresh
     @Override
     public void onPrepareOptionsMenu(Menu menu) {
         menu.findItem(R.id.menu_item_refresh).setVisible(false);
         super.onPrepareOptionsMenu(menu);
     }
+
+
 }
