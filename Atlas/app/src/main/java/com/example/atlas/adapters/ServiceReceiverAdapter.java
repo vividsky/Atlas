1def62961d739e99e2535e71a7d355bcf18ff741
shradhakedia <shradhakedia1998@gmail.com>
2021-12-27 12:52:53 +0530

    implement messaging feature by creating chatrooms.

--- Atlas/app/src/main/java/com/example/atlas/adapters/ServiceReceiverAdapter.java
+++ Atlas/app/src/main/java/com/example/atlas/adapters/ServiceReceiverAdapter.java
@@ -1,6 +1,7 @@
 package com.example.atlas.adapters;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,12 +10,19 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.atlas.Models.Chatroom;
 import com.example.atlas.Models.ServiceProvider;
 import com.example.atlas.Models.ServiceReceiver;
 import com.example.atlas.Models.User;
 import com.example.atlas.R;
+import com.example.atlas.home.MessageFragment;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.List;
 // TODO to implement same as service provider but chatRoom id should be taken care of
@@ -22,11 +30,16 @@ import java.util.List;
 public class ServiceReceiverAdapter extends RecyclerView.Adapter<ServiceReceiverAdapter.ContentViewHolder> {
     static final String TAG = ServiceReceiverAdapter.class.getSimpleName();
 
+    Context context;
     List<ServiceReceiver> serviceReceiver;
     boolean isStarred;
 
-    public ServiceReceiverAdapter(List<ServiceReceiver> serviceReceiver) {
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
+
+    public ServiceReceiverAdapter(List<ServiceReceiver> serviceReceiver, Context context) {
         this.serviceReceiver = serviceReceiver;
+        this.context = context;
     }
 
     @NonNull
@@ -52,19 +65,20 @@ public class ServiceReceiverAdapter extends RecyclerView.Adapter<ServiceReceiver
         TextView name;
         TextView gender;
         TextView address;
-        ImageView contact;
         ImageView starred;
         TextView requirements;
+        ImageView sendMessage;
 
         public ContentViewHolder(@NonNull View itemView) {
             super(itemView);
             name = itemView.findViewById(R.id.sr_tv_name);
             gender = itemView.findViewById(R.id.sr_tv_gender);
             address = itemView.findViewById(R.id.sr_tv_address);
-            contact = itemView.findViewById(R.id.sr_iv_contact);
             starred = itemView.findViewById(R.id.sr_iv_starred);
             requirements = itemView.findViewById(R.id.sr_tv_requirement);
+            sendMessage = itemView.findViewById(R.id.sr_iv_sendMessage);
         }
+
         void bind(int listIndex) {
             ServiceReceiver serviceReceiverDetails = serviceReceiver.get(listIndex);
             User userDetails = serviceReceiverDetails.getUserDetails();
@@ -72,6 +86,47 @@ public class ServiceReceiverAdapter extends RecyclerView.Adapter<ServiceReceiver
             gender.setText(userDetails.getGender());
             address.setText(userDetails.getAddress());
             requirements.setText(serviceReceiverDetails.getRequirements().toString());
+
+            sendMessage.setOnClickListener(view -> {
+                firebaseAuth = FirebaseAuth.getInstance();
+                firebaseFirestore = FirebaseFirestore.getInstance();
+
+                DocumentReference currentUserDocumentReference =
+                        firebaseFirestore.collection("User").document(firebaseAuth.getCurrentUser().getUid());
+
+                currentUserDocumentReference.get()
+                        .addOnCompleteListener(task -> {
+                            String currentUserId = currentUserDocumentReference.getId();
+                            String serviceReceiverId = serviceReceiverDetails.getId();
+                            String uniqueChatroomId = serviceReceiverId + currentUserId;
+                            User user;
+                            if (task.isSuccessful()) {
+                                DocumentReference drChatId = firebaseFirestore.collection("User").document(firebaseAuth.getCurrentUser().getUid())
+                                        .collection("Chatroom").document(uniqueChatroomId);
+                                drChatId.set(new Chatroom(userDetails.getName(), drChatId.getId(), serviceReceiverId));
+
+                                user = task.getResult().toObject(User.class);
+                                firebaseFirestore.collection("User").document(serviceReceiverId)
+                                        .collection("Chatroom").document(drChatId.getId())
+                                        .set(new Chatroom(user.getName(), drChatId.getId(), currentUserId));
+
+
+                                Bundle bundle = new Bundle();
+                                bundle.putString("userId", serviceReceiverId);
+                                bundle.putString("chatId", drChatId.getId());
+
+                                MessageFragment messageFragment = new MessageFragment();
+                                messageFragment.setArguments(bundle);
+
+                                FragmentManager manager = ((AppCompatActivity) context).getSupportFragmentManager();
+                                manager.beginTransaction()
+                                        .replace(R.id.main_activity_container, messageFragment)
+                                        .addToBackStack(null)
+                                        .commit();
+                            }
+                        });
+            });
+
             starred.setOnClickListener(view -> {
                 if (!isStarred) {
                     starred.setImageResource(R.drawable.ic_starred);
