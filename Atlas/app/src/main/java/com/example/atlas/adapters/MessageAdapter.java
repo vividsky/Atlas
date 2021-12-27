818511e5c67b410e91547c1e5c5af6e47eb1e3f4
nidhi <vividsky09@gmail.com>
2021-12-28 00:24:31 +0530

    Creates heterogeneous RecyclerView for MessageFragment

--- Atlas/app/src/main/java/com/example/atlas/adapters/MessageAdapter.java
+++ Atlas/app/src/main/java/com/example/atlas/adapters/MessageAdapter.java
@@ -10,12 +10,18 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.atlas.Models.Message;
 import com.example.atlas.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.List;
 
 public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageViewHolder> {
 
     List<Message> messageList;
+    FirebaseAuth firebaseAuth;
+//    static final int SELF_MESSAGE_TEXT = 0;
+//    static final int OTHER_MESSAGE_TEXT = 1;
+        static final int SELF_MESSAGE_TEXT = 0;
+        static final int OTHER_MESSAGE_TEXT = 1;
 
     public MessageAdapter(List<Message> messageList) {
         this.messageList = messageList;
@@ -24,13 +30,28 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageV
     @NonNull
     @Override
     public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_message, parent, false);
+        View view;
+        if (viewType == SELF_MESSAGE_TEXT) {
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_message_self, parent, false);
+        } else {
+            view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_message_other, parent, false);
+        }
         return new MessageViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MessageViewHolder holder, int position) {
         holder.bind(position);
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+
+        if (messageList.get(position).getSentBy().equals(FirebaseAuth.getInstance().getCurrentUser().getUid())) {
+            return SELF_MESSAGE_TEXT;
+        } else {
+            return OTHER_MESSAGE_TEXT;
+        }
     }
 
     @Override
