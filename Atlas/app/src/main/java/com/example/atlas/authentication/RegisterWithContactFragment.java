9f4eb0b450b3cfc499413074c00209ee6b26dd25
shradhakedia <shradhakedia1998@gmail.com>
2021-12-30 00:15:19 +0530

    final touch to phone authentication

--- Atlas/app/src/main/java/com/example/atlas/authentication/RegisterWithContactFragment.java
+++ Atlas/app/src/main/java/com/example/atlas/authentication/RegisterWithContactFragment.java
@@ -2,12 +2,12 @@ package com.example.atlas.authentication;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
-import android.text.TextUtils;
+import android.text.Editable;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextWatcher;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,17 +15,18 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.example.atlas.Models.User;
 import com.example.atlas.R;
 import com.example.atlas.Utils;
-import com.example.atlas.home.MainActivity;
-import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.FirebaseException;
-import com.google.firebase.FirebaseTooManyRequestsException;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthOptions;
 import com.google.firebase.auth.PhoneAuthProvider;
@@ -38,7 +39,7 @@ public class RegisterWithContactFragment extends Fragment {
     private static final String TAG = RegisterWithContactFragment.class.getSimpleName();
 
     private FirebaseAuth firebaseAuth;
-    PhoneAuthProvider.ForceResendingToken mResendToken;
+    private PhoneAuthProvider.ForceResendingToken mResendToken;
     private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
 
     private String mVerificationId;
@@ -46,7 +47,7 @@ public class RegisterWithContactFragment extends Fragment {
     private String otp;
 
     private EditText mGetOTP;
-    private TextInputLayout mGetContactNumber;
+    private EditText mGetContactNumber;
     private Button mSendOTPButton;
     private Button mLoginButton;
     private ProgressBar mProgressBar;
@@ -80,6 +81,17 @@ public class RegisterWithContactFragment extends Fragment {
                 //     user action.
                 Log.d(TAG, "onVerificationCompleted:" + credential);
 
+                final String code = credential.getSmsCode();
+
+                // checking if the code
+                // is null or not.
+                if (code != null) {
+                    // if the code is not null then
+                    // we are setting that code to
+                    // our OTP edittext field.
+                    mGetOTP.setText(code);
+                    mProgressBar.setVisibility(View.VISIBLE);
+                }
                 signInWithPhoneAuthCredential(credential);
             }
 
@@ -88,6 +100,9 @@ public class RegisterWithContactFragment extends Fragment {
                 // This callback is invoked in an invalid request for verification is made,
                 // for instance if the the phone number format is not valid.
                 mProgressBar.setVisibility(View.GONE);
+                mSendOTPButton.setVisibility(View.VISIBLE);
+                mGetOTP.setVisibility(View.GONE);
+                mLoginButton.setVisibility(View.GONE);
                 Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
                 Log.w(TAG, "onVerificationFailed", e);
 
@@ -103,25 +118,63 @@ public class RegisterWithContactFragment extends Fragment {
 
                 // Save verification ID and resending token so we can use them later
                 mVerificationId = verificationId;
-                // TODO use resend token method
-//                mResendToken = token;
+                mResendToken = token;
+            }
+
+            @Override
+            public void onCodeAutoRetrievalTimeOut(@NonNull String s) {
+                super.onCodeAutoRetrievalTimeOut(s);
+                Log.i(TAG, "onCodeAutoRetrievalTimeOut: Sms auto retrieval timed-out");
+                Toast.makeText(getActivity(), "Sms auto retrieval timed-out", Toast.LENGTH_SHORT).show();
+                mProgressBar.setVisibility(View.GONE);
             }
         };
 
-        // TODO Switch visiblity to gone
-        mGetContactNumber.setOnClickListener(view3 -> {
-            mSendOTPButton.setVisibility(View.VISIBLE);
-            mGetOTP.setVisibility(View.GONE);
-            mLoginButton.setVisibility(View.GONE);
+        TextView mResendOtp = view.findViewById(R.id.tv_resend_otp);
+        String resendOtpText = "Didn't receive OTP? Resend.";
+        SpannableString ss = new SpannableString(resendOtpText);
+
+        // creating clickable span to be implemented as a link
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            public void onClick(View widget) {
+                resendVerificationCode(phoneNumber, mResendToken);
+            }
+        };
+
+        // setting the part of string to be act as a link
+        ss.setSpan(clickableSpan, resendOtpText.indexOf("Resend"), resendOtpText.indexOf('.'), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        mResendOtp.setText(ss);
+        mResendOtp.setMovementMethod(LinkMovementMethod.getInstance());
+
+
+        mGetContactNumber.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence text, int start,
+                                      int before, int count) {
+                mSendOTPButton.setVisibility(View.VISIBLE);
+                mGetOTP.setVisibility(View.GONE);
+                mLoginButton.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
         });
 
         mSendOTPButton.setOnClickListener(view1 -> {
 
-            phoneNumber = "+91" + mGetContactNumber.getEditText().getText().toString();
+            phoneNumber = "+91" + mGetContactNumber.getText().toString();
             mSendOTPButton.setVisibility(View.GONE);
             mGetOTP.setVisibility(View.VISIBLE);
             mLoginButton.setVisibility(View.VISIBLE);
-            mProgressBar.setVisibility(View.VISIBLE);
 
             // To send OTP to the given number
             startPhoneNumberVerification(phoneNumber);
@@ -129,6 +182,7 @@ public class RegisterWithContactFragment extends Fragment {
 
         mLoginButton.setOnClickListener(view2 -> {
             otp = mGetOTP.getText().toString();
+            mProgressBar.setVisibility(View.VISIBLE);
             if (otp.isEmpty()) {
                 Toast.makeText(getContext(), "OTP is required", Toast.LENGTH_SHORT).show();
             } else if (otp.length() != 6) {
@@ -137,7 +191,13 @@ public class RegisterWithContactFragment extends Fragment {
                 // While entering OTP manually
                 // mVerificationId -> by FirebaseAuth (onCodeSent method)
                 // otp -> entered by user
-                signInWithPhoneAuthCredential(verifyPhoneNumberWithCode(mVerificationId, otp));
+                try {
+                    signInWithPhoneAuthCredential(verifyPhoneNumberWithCode(mVerificationId, otp));
+                } catch (Exception e) {
+                    Log.d(TAG, "onViewCreated: " + e.getMessage());
+                    mProgressBar.setVisibility(View.GONE);
+                    Toast.makeText(getContext(), "Invalid OTP", Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
@@ -156,13 +216,15 @@ public class RegisterWithContactFragment extends Fragment {
     private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
         firebaseAuth.signInWithCredential(credential)
                 .addOnCompleteListener(task -> {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithCredential:success");
-                            updateUI();
-                        } else {
-                            Log.w(TAG, "signInWithCredential:failure", task.getException());
-                        }
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithCredential:success");
+                        updateUI();
+                    } else {
+                        Toast.makeText(getContext(), "Invalid OTP, Try Again.", Toast.LENGTH_LONG).show();
+                        mProgressBar.setVisibility(View.GONE);
+                        Log.w(TAG, "signInWithCredential:failure", task.getException());
+                    }
                 });
     }
 
@@ -171,23 +233,19 @@ public class RegisterWithContactFragment extends Fragment {
     }
 
     private void updateUI() {
-        Log.i(TAG, Utils.getCurrentFirebaseUser().getPhoneNumber());
+
         DocumentReference currentUserDR = Utils.getCurrentUserDocumentReference();
         currentUserDR.get().addOnCompleteListener(task -> {
             User user = task.getResult().toObject(User.class);
-            if (TextUtils.isEmpty(user.getContact())) {
-                User mUser = new User(firebaseAuth.getCurrentUser().getUid(), "", phoneNumber);
-
-                Utils.getCurrentUserDocumentReference().set(mUser)
+            if (user == null) {
+                User mUser = new User(firebaseAuth.getCurrentUser().getUid(), null, phoneNumber);
+                currentUserDR.set(mUser)
                         .addOnCompleteListener(isUserCreatedTask -> {
-
                             // once done with sign up, progress bar visibility set to GONE
                             mProgressBar.setVisibility(View.GONE);
-
                             if (isUserCreatedTask.isSuccessful()) {
                                 Toast.makeText(getContext(), "Sign up successful.",
                                         Toast.LENGTH_SHORT).show();
-
                                 // working of moving from signUp page to userDetails Page
                                 getParentFragmentManager()
                                         .beginTransaction()
